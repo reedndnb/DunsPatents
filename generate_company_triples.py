2074088f17f3247445c7a27bdfa4b66414e59b29
natereed <reedn@dnb.com>
2016-03-24 19:42:33 -0500

    PIC16-347: Cleanup companies RDF

--- generate_company_triples.py
+++ generate_company_triples.py
@@ -20,27 +20,31 @@ def print_triple_literal(subject, predicate, value):
     triple = triple_literal(subject, predicate, value)
     if triple:
         print(triple)
-  
-def company_triples(duns, name, pub_id, relationship):
-    t1 = "<http://dnb.com/company/%s>, <http://dnb.com/patents/publication/%s>, <'http://dnb.com/patent/pubid/%s'>" % (duns, relationship, pub_id)
-    t2 = "<http://dnb.com/company/%s>, <http://dnb.com/company/name>, '%s'" % (duns, name)
-    return [t1, t2]
+
+def write_company_triples(company):
+    company_uri = COMPANY_URI % company['duns']
+    pub_uri = PUB_URI % company['pub_id']
+    print_triple_object(company_uri,
+                        'http://dnb.com/patents/publication/' + company['relationship'],
+                        pub_uri)
+    print_triple_literal(company_uri,
+                         'http://dnb.com/company/name',
+                         company['name'])
 
 with open("companies-appended-sample.csv", "r") as companies_in:
     reader = csv.DictReader(companies_in)
-    with open("companies-triples.rdf", "w") as companies_out:
-        for row in reader:
-            pub_id = row['pub-id']
-            test = 'http://dnb.com/patent/publication-id/%s' % pub_id
-            print "Test: %s" % test
-            print(pub_id)
-            if not row.get('duns'):
-                continue
+    for row in reader:
+        #print "---- ROW: %s ------" % str(row)
+        pub_id = row['pub-id']
+        test = 'http://dnb.com/patent/publication-id/%s' % pub_id
+        if not row.get('duns'):
+            #print "No Duns -- skipping..."
+            continue
+        #print '--- %s, %s ---- ' % (row.get('duns'), row.get('name'))
 
-            for triple in company_triples(row['duns'], row['name'], pub_id, row['relationship']):
-                companies_out.write(triple)
-                companies_out.write('\n')
+        company = {'duns' : row['duns'],
+                   'name' : row['name'],
+                   'relationship' : row['relationship'],
+                   'pub_id' : pub_id}
 
-    #with open("pub-triples.rdf", "w") as pubs_out:
-        #for row in reader:
-        #    row['cit-number']
+        write_company_triples(company)
